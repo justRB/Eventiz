4078b6addc187be9850d66e7da7807d5870777b8
justRB <bietrix.romuald@gmail.com>
2023-02-27 10:21:49 +0100

    app entry

---
+++ lib/pages/connection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Connection extends StatelessWidget {
+  const Connection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('page connexion');
+  }
+}
