182a49e30451957218e3f3022f6cb7e99a043154
Beinstore <beinstore@mbpdebeinstore.home>
2023-03-17 09:43:07 +0100

    modif profil page

--- lib/pages/profil.dart
+++ lib/pages/profil.dart
@@ -1,3 +1,5 @@
+import 'package:app/components/nav_bar.dart';
+import 'package:app/components/top_bar.dart';
 import 'package:flutter/material.dart';
 
 class Profil extends StatefulWidget {
@@ -10,6 +12,96 @@ class Profil extends StatefulWidget {
 class _ProfilState extends State<Profil> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: const TopBar(title: 'Profile'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 20),
+          const CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('assets/profile_picture.png'),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'John Doe',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Compte crée en 2019',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: const [
+                  Text(
+                    '100',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Événements Crées',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: const [
+                  Text(
+                    '200',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Événements Participés',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Container(
+            color: Colors.blue,
+            child: Column(
+              children: [
+                Text(
+                  'Information Supplémantaire'
+                )
+                Column(children: const [
+                  Text(
+                    'Nombre d\'événements créés',
+                  )
+                ],)
+
+              ]
+            ),
+
+          ),
+        ],
+      ),
+      bottomNavigationBar: const NavBar(),
+    );
   }
 }
